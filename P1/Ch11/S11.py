d822050fb3b96bd61bc4d596efd775b51f38afa4
muthukamalan <muthukamalan98@gmail.com>
2024-08-28 23:23:19 +0530

    iterable and iterator II  --init

---
+++ P1/Ch11/S11.py
@@ -0,0 +1,32 @@
+# Iterable
+
+
+## class with In memory variable
+## class with lazy calc variable (on-request)
+
+
+###
+#### range 
+#### with(open(),mode='a') as f
+#### enumerate
+
+
+## Iterator with __getitem__
+
+
+## is __iter__ enough to make iterable
+
+
+## iter on  callable 
+help(iter)
+## design of for-loop
+
+
+
+## reversed iterable  
+## reversed on custom sequence types
+
+
+
+
+# iterable /iterator
